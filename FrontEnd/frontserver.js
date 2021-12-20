4c35b510638c93fd3f110a7426b55e528f8118df
Narciso Ferreira <supereor@gmail.com>
2021-12-20 19:08:40 -0300

    Quase

--- FrontEnd/frontserver.js
+++ FrontEnd/frontserver.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(__dirname + '/src/pages'));
+app.use(express.static(__dirname + '/src'));
 
 app.listen(port || 5500, () => {
     console.log(`Server frontend rodando na porta ${port}`)
