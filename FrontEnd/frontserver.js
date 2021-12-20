fcbf9bec0d7175cddea5e0e8c1f1183ab6c13603
Narciso Ferreira <supereor@gmail.com>
2021-12-20 18:46:06 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ FrontEnd/frontserver.js
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express();
+const port = 5500;
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(__dirname + '/src/pages'));
+
+app.listen(port || 5500, () => {
+    console.log(`Server frontend rodando na porta ${port}`)
+})
