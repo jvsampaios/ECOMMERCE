1f3b4b2c3c13b9007e88281e9a75856197830ee9
The_King_Davis <davispenha@gmail.com>
2022-01-16 09:20:38 -0300

    Ajustes no Promise-chain dos seeds

--- BackEnd/src/database/seeds/index.ts
+++ BackEnd/src/database/seeds/index.ts
@@ -1,5 +1,3 @@
-import {v4 as uuid4} from 'uuid'
-
 import {createSeedUserAdmin} from './admin'
 import { createSeedCategory } from './category'
 import { createSeedProduct } from './product'
@@ -10,15 +8,9 @@ const product_uuid='62981309-9d8a-4ac3-95c7-a25212f49f02'
 const category_uuid='e35089d5-e50c-425f-9eba-9effcc77c38e'
 
 createSeedUserAdmin(UserUUID)
-.then(_=>{
-    return createSeedProduct(product_uuid)
-})
-.then(_=>{
-    return createSeedCategory(category_uuid)
-})
-.then(_=>{
-    return createSeedProductCategory(product_uuid,category_uuid)
-})
+.then(_=>createSeedProduct(product_uuid))
+.then(_=>createSeedCategory(category_uuid))
+.then(_=> createSeedProductCategory(product_uuid,category_uuid))
 .catch(err=>{
     console.log(err)
 })
